ffb03db73a3a79adddb6da1dd700859f6b7ca4e2
wuchun1103 <1186527291@qq.com>
2021-08-16 10:19:59 +0800

    算法和爬虫

--- base_front/src/utils/global.js
+++ base_front/src/utils/global.js
@@ -7,7 +7,7 @@
 // 后台管理系统服务器地址
 
 // export const baseUrl = 'http://10.10.10.87:8002'
- export const baseUrl = 'http://localhost:8008'
+ export const baseUrl = 'http://192.168.2.28:8008'
 
 export default {
   baseUrl
